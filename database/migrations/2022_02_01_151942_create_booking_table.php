29346f887487b84c3b229bdb1cbf955ec5445c5f
ITstep <itstep@itstep.loc>
2022-02-01 21:35:11 +0600

    migrations, tables

---
+++ database/migrations/2022_02_01_151942_create_booking_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBookingTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('booking', function (Blueprint $table) {
+            $table->id();
+            $table->integer('productId')->nullable();
+            $table->foreign('productId')
+                ->references('id')->on('product')->onDelete('cascade');
+
+            $table->date('startDate');
+            $table->date('endDate')->nullable();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('booking');
+    }
+}
